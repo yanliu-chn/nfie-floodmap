95a400e80c7a9737caf688038c012e6ccef9a57b
ahmetartu <ayild@illinois.edu>
2016-05-01 18:28:59 -0500

    -changed algorithm that now checks the topology of the flows using fromNode and toNode attributes to find the inlet points

--- src/find_inlets/src/find_inlets.cpp
+++ src/find_inlets/src/find_inlets.cpp
@@ -53,10 +53,17 @@ struct LineList {
     Point ePoint;
 };
 
+struct LineList2 {
+    Point sPoint;
+    Point ePoint;
+    long fromPoint;
+};
+
 static string shapefile;
 static string danglefile;
 
 static void findDanglePoints();
+static void findDanglePointsV2();
 static bool inLine(Point A, Point B, Point C);
 static double distance(Point A, Point B);
 
@@ -68,6 +75,136 @@ bool inLine(Point A, Point B, Point C) {
     if (distance(A, C) + distance(B, C) == distance(A, B))
         return true;
     return false;
+}
+
+void findDanglePointsV2() {
+    GDALDatasetH hDSFlow;
+    OGRLayerH hLayerFlow;
+
+    hDSFlow = GDALOpenEx(shapefile.c_str(), GDAL_OF_VECTOR, NULL, NULL, NULL);
+    if (hDSFlow == NULL) {
+        cerr << "ERROR: Failed to open the file: " << shapefile << endl;
+        exit(1);
+    }
+
+    hLayerFlow = GDALDatasetGetLayer(hDSFlow, 0);
+
+    if (hLayerFlow == NULL) {
+        cerr << "ERROR: Failed to open layer of the shapefile: " << shapefile << endl;
+        exit(1);
+    }
+
+    OGRSpatialReferenceH hSpatialRefFlowLines = OGR_L_GetSpatialRef(hLayerFlow);
+
+    OSRReference(hSpatialRefFlowLines);
+    
+    vector<LineList2> lineStrings;
+    unordered_set<long> toPoints;
+
+    OGR_L_ResetReading(hLayerFlow);
+
+    OGRFeatureH hFeature;
+
+    while ((hFeature = OGR_L_GetNextFeature(hLayerFlow)) != NULL) {
+        OGRGeometryH hGeometry;
+        hGeometry = OGR_F_GetGeometryRef(hFeature);
+
+        OGRFeatureDefnH hFDefn;
+        int iField;
+        hFDefn = OGR_L_GetLayerDefn(hLayerFlow);
+        int fromPoint = OGR_F_GetFieldAsInteger(hFeature, 14);
+        int toPoint = OGR_F_GetFieldAsInteger(hFeature, 15);
+        
+
+        if (hGeometry != NULL) {
+            OGRwkbGeometryType gType = wkbFlatten(OGR_G_GetGeometryType(hGeometry));
+            if (gType == wkbLineString) {
+                int pointCount = OGR_G_GetPointCount(hGeometry);
+                double* xBuffer = (double*) malloc(sizeof (double) * pointCount);
+                double* yBuffer = (double*) malloc(sizeof (double) * pointCount);
+                int pc = OGR_G_GetPoints(hGeometry, xBuffer, sizeof (double), yBuffer, sizeof (double), NULL, 0);
+
+                Point tmpStart = Point(xBuffer[0], yBuffer[0]);
+                Point tmpEnd = Point(xBuffer[pc - 1], yBuffer[pc - 1]);
+
+                LineList2 line;
+                line.sPoint = tmpStart;
+                line.ePoint = tmpEnd;
+                line.fromPoint = fromPoint;
+                
+                lineStrings.push_back(line);
+                
+                free(xBuffer);
+                free(yBuffer);
+                
+                auto got = toPoints.find (toPoint);
+
+                if (got == toPoints.end())
+                    toPoints.insert(toPoint);
+            }
+        }
+
+        OGR_F_Destroy(hFeature);
+    }
+    GDALClose(hDSFlow);
+
+    const char *pszDriverName = "ESRI Shapefile";
+    GDALDriver *poDriver;
+    poDriver = (GDALDriver*) GDALGetDriverByName(pszDriverName);
+    if (poDriver == NULL) {
+        cerr << "ERROR: " << pszDriverName << " driver is not available" << endl;
+        exit(1);
+    }
+
+    GDALDatasetH poDS;
+
+    poDS = GDALCreate(poDriver, danglefile.c_str(), 0, 0, 0, GDT_Unknown, NULL);
+    if (poDS == NULL) {
+        cerr << "ERROR: Failed to create file: " << danglefile << endl;
+        exit(1);
+    }
+
+    OGRLayerH hLayerOut;
+    hLayerOut = GDALDatasetCreateLayer(poDS, "Dangles", hSpatialRefFlowLines, wkbPoint, NULL);
+    if (hLayerOut == NULL) {
+        cerr << "ERROR: Failed to create Dangles layer in file: " << danglefile << endl;
+        exit(1);
+    }
+    
+    int i, j, k;
+
+    vector<Point> myinlets;
+
+    for (i = 0; i < lineStrings.size(); ++i) {
+        LineList2 line = lineStrings[i];
+
+        auto got = toPoints.find (line.fromPoint);
+
+        if (got == toPoints.end())
+            myinlets.push_back(line.sPoint);
+    }
+
+    for (i = 0; i < myinlets.size(); ++i) {
+        Point a = myinlets[i];
+
+        OGRFeatureH hFeature;
+        hFeature = OGR_F_Create(OGR_L_GetLayerDefn(hLayerOut));
+
+        OGRGeometryH hPt;
+        hPt = OGR_G_CreateGeometry(wkbPoint);
+        OGR_G_SetPoint_2D(hPt, 0, a.x, a.y);
+        OGR_F_SetGeometry(hFeature, hPt);
+        OGR_G_DestroyGeometry(hPt);
+
+        if (OGR_L_CreateFeature(hLayerOut, hFeature) != OGRERR_NONE) {
+            cerr << "ERROR: Failed to create feature in file: " << danglefile << endl;
+            exit(1);
+        }
+
+        OGR_F_Destroy(hFeature);
+    }
+
+    GDALClose(poDS);
 }
 
 static void findDanglePoints() {
@@ -234,7 +371,7 @@ int main(int argc, char** argv) {
         exit(1);
     }
 
-    findDanglePoints();
+    findDanglePointsV2();
     return 0;
 }
 
